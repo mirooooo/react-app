ffd07bdd85297d0caa4b5b5c6734435c1e3e6421
Miro <mr.mladenoff@gmail.com>
2018-11-24 19:28:46 -0800

    Multiple Components in Sync

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {
+  Component
+} from 'react';
+import NavBar from './components/navbar';
 import './App.css';
+import Counters from './components/counters';
+
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    return ( <
+      React.Fragment >
+      <
+      NavBar > < /NavBar> <
+      main className = 'container' >
+      <
+      Counters > < /Counters> < /
+      main >
+      <
+      /React.Fragment>
     );
   }
 }
---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <nav className="navber navbar-light bg-light">
+        <a href="#" className="navbar-brand">
+          Navbar
+        </a>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
