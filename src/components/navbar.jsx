9c258072641373651c48e928251a3d4024c0efc3
Miro <mr.mladenoff@gmail.com>
2018-11-26 15:22:48 -0800

    Destructuring Arguments

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,17 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navber navbar-light bg-light">
-        <a href="#" className="navbar-brand">
-          Navbar{" "}
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+//Stateless Functional Component -sfc
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navber navbar-light bg-light">
+      <a href="#" className="navbar-brand">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
